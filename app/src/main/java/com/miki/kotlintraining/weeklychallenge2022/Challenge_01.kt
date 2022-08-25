b48debe48f3b8e64b14aa4e2c736aeac88608364
SrMiki <miguelangelbercianorodriguez@gmail.com>
2022-08-25 15:16:38 +0200

    challenge_01

--- app/src/main/java/com/miki/kotlintraining/weeklychallenge2022/Challenge_01.kt
+++ app/src/main/java/com/miki/kotlintraining/weeklychallenge2022/Challenge_01.kt
@@ -12,3 +12,37 @@ package com.miki.kotlintraining.weeklychallenge2022
  * Dos palabras exactamente iguales no son anagrama.
  *
  */
+
+fun main() {
+
+    println(isAnagrama("ola", "lado")) //false
+    println(isAnagrama("rata", "tara")) // true
+    println(isAnagrama("costa", "tacos")) //true
+    println(isAnagrama("olor", "color")) //false
+    println(isAnagrama("amor", "amor")) //false (equals!)
+    println(isAnagrama("roma", "amor")) //true
+
+
+}
+
+private fun isAnagrama(s1: String, s2: String): Boolean {
+
+    if (s1.length != s2.length || s1.lowercase().equals(s2.lowercase())) {
+        return false;
+    }
+
+    for (a in s1.lowercase()) {
+        if (!s2.lowercase().contains(a)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+private fun isAnagramaV2(wordOne: String, wordTwo: String): Boolean {
+    if (wordOne.lowercase() == wordTwo.lowercase()) {
+        return false
+    }
+    return wordOne.lowercase().toCharArray().sortedArray()
+        .contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
+}
