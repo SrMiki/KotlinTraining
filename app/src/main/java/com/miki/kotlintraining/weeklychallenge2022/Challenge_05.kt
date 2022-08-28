a3b1e3b8f4d2f14bc592adf83ae375ef8eae8fe8
SrMiki <miguelangelbercianorodriguez@gmail.com>
2022-08-28 09:37:00 +0200

    retos 4, 5?, 6

---
+++ app/src/main/java/com/miki/kotlintraining/weeklychallenge2022/Challenge_05.kt
@@ -0,0 +1,16 @@
+package com.miki.kotlintraining.weeklychallenge2022
+
+/*
+* Reto #5 | ASPECT RATIO DE UNA IMAGEN
+*
+* Enunciado: Crea un programa que se encargue de calcular el aspect ratio de una imagen a partir de una url.
+* - Url de ejemplo: https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png
+* - Por ratio hacemos referencia por ejemplo a los "16:9" de una imagen de 1920*1080px.
+*
+*/
+
+fun main() {
+
+    //TODO!!
+
+}
---
+++ app/src/main/java/com/miki/kotlintraining/weeklychallenge2022/Challenge_06.kt
@@ -0,0 +1,32 @@
+package com.miki.kotlintraining.weeklychallenge2022
+
+/*
+ * Reto #6 | INVIRTIENDO CADENAS
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones
+ * propias del lenguaje que lo hagan de forma automática.
+ *
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ */
+
+fun main() {
+
+    println(reverseString(""))
+    println(reverseString("h"))
+    println(reverseString("holamundo"))
+    println(reverseString("odnum aloH"))
+    println(reverseString("frase normal"))
+    println(reverseString("reconocer"))
+
+}
+
+fun reverseString(stringToReverse: String): String {
+    var aux = ""
+
+    for (i in stringToReverse.length..0) {
+        aux += stringToReverse[i]
+    }
+    return aux
+
+}
